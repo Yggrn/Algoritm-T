b5e582a11c19ef9092536293197cdfa13ea7922b
skip59 <skip59@yandex.ru>
2023-01-08 20:22:16 +0500

    NewWeb

---
+++ AlgoritmWeb/AlgoritmWeb/Models/TokenKey.cs
@@ -0,0 +1,9 @@
+﻿namespace AlgoritmWeb.Models
+{
+    public class TokenKey
+    {
+            public  Guid correlationId { get; set; }
+        public string token { get; set; } = null!;
+     
+    }
+}
---
+++ AlgoritmWeb/AlgoritmWeb/Models/TreeViewNode.cs
@@ -0,0 +1,9 @@
+﻿namespace AlgoritmWeb.Models
+{
+    public class TreeViewNode
+    {
+        public string id { get; set; }
+        public string parent { get; set; }
+        public string text { get; set; }
+    }
+}
